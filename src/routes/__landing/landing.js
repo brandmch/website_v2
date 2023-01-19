1457313d538158f2d12085a4ceada7c56844634a
brandmch <101467867+brandmch@users.noreply.github.com>
2023-01-18 23:34:52 -0500

    mui looking good

---
+++ src/routes/__landing/landing.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./landing.css";
+import AppBarCustom from "../../components/appbar";
+
+const Landing = () => {
+  return (
+    <div className="Landing">
+      <AppBarCustom />
+    </div>
+  );
+};
+
+export default Landing;
--- src/routes/_landing/landing.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./landing.css";
-
-const Landing = () => {
-  return <div className="App"></div>;
-};
-
-export default Landing;
