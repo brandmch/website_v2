f18a50a7c3c92eeed5b40768e581d1aeafcf9bea
brandmch <101467867+brandmch@users.noreply.github.com>
2023-01-19 22:21:48 -0500

    added notes, make code readable

--- src/routes/landing.js
+++ src/routes/landing.js
@@ -10,32 +10,67 @@ const Landing = () => {
         {/*  */}
         {/* LEFT COLUMN */}
         {/*  */}
+
         <Box sx={{ flex: 2, margin: "10px" }}>
+          {/* CONTACT INFO */}
           <Paper elevation={2} sx={{ height: 100, width: "100%" }}>
-            <Typography variant="h4">Brandon McHugh</Typography>
+            <Typography variant="h4" align="center">
+              Brandon McHugh
+            </Typography>
           </Paper>
+
+          {/* SKILLS */}
           <Paper
             elevation={2}
             sx={{ height: 100, width: "100%", marginTop: "20px" }}
-          ></Paper>
+          >
+            <Typography variant="h4" align="center">
+              Skills
+            </Typography>
+          </Paper>
+
+          {/* CERTIFICATIONS? */}
           <Paper
             elevation={2}
             sx={{ height: 100, width: "100%", marginTop: "20px" }}
-          ></Paper>
+          >
+            <Typography variant="h4" align="center">
+              Certifications?
+            </Typography>
+          </Paper>
         </Box>
+
         {/*  */}
         {/* RIGHT COLUMN */}
         {/*  */}
+
         <Box sx={{ flex: 5, margin: "10px" }}>
-          <Paper elevation={2} sx={{ height: 100, width: "100%" }}></Paper>
+          {/* SUMMARY */}
+          <Paper elevation={2} sx={{ height: 100, width: "100%" }}>
+            <Typography variant="h4" align="center">
+              Summary
+            </Typography>
+          </Paper>
+
+          {/* EXPERIENCE */}
           <Paper
             elevation={2}
             sx={{ height: 100, width: "100%", marginTop: "20px" }}
-          ></Paper>{" "}
+          >
+            <Typography variant="h4" align="center">
+              Experience
+            </Typography>
+          </Paper>
+
+          {/* EDUCATION */}
           <Paper
             elevation={2}
             sx={{ height: 100, width: "100%", marginTop: "20px" }}
-          ></Paper>
+          >
+            <Typography variant="h4" align="center">
+              Education
+            </Typography>
+          </Paper>
         </Box>
       </Box>
     </div>
