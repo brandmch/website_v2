5dc3f857169585ad94210672a6943375d300c94b
brandmch <101467867+brandmch@users.noreply.github.com>
2023-01-19 23:34:07 -0500

    fixed margins

--- src/routes/landing.js
+++ src/routes/landing.js
@@ -21,124 +21,128 @@ const Landing = () => {
 
         <Box flex={2} margin={2}>
           {/* CONTACT INFO */}
-          <Box marginBottom={2}>
-            <Paper elevation={2}>
-              <Box paddingTop={2}>
+          <Box marginBottom={4}>
+            <Paper elevation={4}>
+              <Box padding={2.5}>
                 <Typography variant="h4" align="center" gutterBottom>
                   Brandon McHugh
                 </Typography>
+
+                <hr />
+                <Typography gutterBottom>
+                  <WorkIcon />
+                  Software Developer
+                </Typography>
+                <Typography gutterBottom>
+                  <SchoolIcon />
+                  Bachelor of Science
+                </Typography>
+                <Typography gutterBottom>
+                  <HomeIcon />
+                  Rockaway, New Jersey
+                </Typography>
+                <Typography gutterBottom>
+                  <CallIcon />
+                  Contact me
+                </Typography>
               </Box>
-              <hr />
-              <Typography gutterBottom>
-                <WorkIcon />
-                Software Developer
-              </Typography>
-              <Typography gutterBottom>
-                <SchoolIcon />
-                Bachelor of Science
-              </Typography>
-              <Typography gutterBottom>
-                <HomeIcon />
-                Rockaway, New Jersey
-              </Typography>
-              <Typography gutterBottom>
-                <CallIcon />
-                Contact me
-              </Typography>
             </Paper>
           </Box>
 
           {/* SKILLS */}
-          <Box marginBottom={2} marginTop={2}>
-            <Paper elevation={2}>
-              <Box paddingTop={2}>
+          <Box marginBottom={4} marginTop={4}>
+            <Paper elevation={4}>
+              <Box padding={2.5}>
                 <Typography variant="h4" align="center" gutterBottom>
                   Languages and Frameworks
                 </Typography>
+
+                <hr />
+                <Typography>
+                  <JavascriptIcon fontSize="medium" />
+                  JavaScript
+                </Typography>
+                <Typography>
+                  <CodeIcon fontSize="medium" />
+                  React
+                </Typography>
+                <Typography>
+                  <CodeIcon fontSize="medium" />
+                  React Native
+                </Typography>
+                <Typography>
+                  <HtmlIcon fontSize="medium" />
+                  HTML
+                </Typography>
+                <Typography>
+                  <CssIcon fontSize="medium" />
+                  CSS
+                </Typography>
+                <Typography>
+                  <CodeIcon fontSize="medium" />
+                  Node.js
+                </Typography>
+                <Typography>
+                  <CodeIcon fontSize="medium" />
+                  VS Code
+                </Typography>
               </Box>
-              <hr />
-              <Typography>
-                <JavascriptIcon fontSize="medium" />
-                JavaScript
-              </Typography>
-              <Typography>
-                <CodeIcon fontSize="medium" />
-                React
-              </Typography>
-              <Typography>
-                <CodeIcon fontSize="medium" />
-                React Native
-              </Typography>
-              <Typography>
-                <HtmlIcon fontSize="medium" />
-                HTML
-              </Typography>
-              <Typography>
-                <CssIcon fontSize="medium" />
-                CSS
-              </Typography>
-              <Typography>
-                <CodeIcon fontSize="medium" />
-                Node.js
-              </Typography>
-              <Typography>
-                <CodeIcon fontSize="medium" />
-                VS Code
-              </Typography>
             </Paper>
           </Box>
 
           {/* CERTIFICATIONS? */}
-          <Box marginBottom={2} marginTop={2}>
-            <Paper elevation={2}>
-              <Box paddingTop={2}>
+          <Box marginBottom={4} marginTop={4}>
+            <Paper elevation={4}>
+              <Box padding={2.5}>
                 <Typography variant="h4" align="center" gutterBottom>
                   Certifications?
                 </Typography>
+
+                <hr />
               </Box>
-              <hr />
             </Paper>
           </Box>
         </Box>
-
         {/*  */}
         {/* RIGHT COLUMN */}
         {/*  */}
 
         <Box flex={5} margin={2}>
           {/* SUMMARY */}
-          <Box marginBottom={2}>
-            <Paper>
-              <Box paddingTop={2}>
+          <Box marginBottom={4}>
+            <Paper elevation={4}>
+              <Box padding={2.5}>
                 <Typography variant="h4" align="center" gutterBottom>
                   Summary
                 </Typography>
+                <hr />
               </Box>
-              <hr />
             </Paper>
           </Box>
 
           {/* EXPERIENCE */}
-          <Box marginBottom={2} marginTop={2}>
-            <Paper elevation={2}>
-              <Box paddingTop={2}>
+          <Box marginBottom={4} marginTop={4}>
+            <Paper elevation={4}>
+              <Box padding={2.5}>
                 <Typography variant="h4" align="center" gutterBottom>
                   Experience
                 </Typography>
+
+                <hr />
               </Box>
-              <hr />
             </Paper>
           </Box>
 
           {/* EDUCATION */}
-          <Box marginBottom={2} marginTop={2}>
-            <Paper elevation={2}>
-              <Box paddingTop={2}>
+          <Box marginBottom={4} marginTop={4}>
+            <Paper elevation={4}>
+              <Box padding={2.5}>
                 <Typography variant="h4" align="center" gutterBottom>
                   Education
                 </Typography>
+
+                <hr />
               </Box>
-              <hr />
             </Paper>
           </Box>
         </Box>
