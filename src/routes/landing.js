5a6c3b79b74a23854576b0d1d7e82042b4148ddb
brandmch <101467867+brandmch@users.noreply.github.com>
2023-01-19 22:16:37 -0500

    layout done w boxes

--- src/routes/landing.js
+++ src/routes/landing.js
@@ -1,3 +1,4 @@
+import { Paper, Box, Typography } from "@mui/material";
 import React from "react";
 import AppBarCustom from "../components/appbar";
 
@@ -5,7 +6,38 @@ const Landing = () => {
   return (
     <div>
       <AppBarCustom />
-      <h1>LANDING!</h1>
+      <Box sx={{ display: "flex", marginTop: "10px" }}>
+        {/*  */}
+        {/* LEFT COLUMN */}
+        {/*  */}
+        <Box sx={{ flex: 2, margin: "10px" }}>
+          <Paper elevation={2} sx={{ height: 100, width: "100%" }}>
+            <Typography variant="h4">Brandon McHugh</Typography>
+          </Paper>
+          <Paper
+            elevation={2}
+            sx={{ height: 100, width: "100%", marginTop: "20px" }}
+          ></Paper>
+          <Paper
+            elevation={2}
+            sx={{ height: 100, width: "100%", marginTop: "20px" }}
+          ></Paper>
+        </Box>
+        {/*  */}
+        {/* RIGHT COLUMN */}
+        {/*  */}
+        <Box sx={{ flex: 5, margin: "10px" }}>
+          <Paper elevation={2} sx={{ height: 100, width: "100%" }}></Paper>
+          <Paper
+            elevation={2}
+            sx={{ height: 100, width: "100%", marginTop: "20px" }}
+          ></Paper>{" "}
+          <Paper
+            elevation={2}
+            sx={{ height: 100, width: "100%", marginTop: "20px" }}
+          ></Paper>
+        </Box>
+      </Box>
     </div>
   );
 };
