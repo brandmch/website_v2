7e0555391169561573cf6b22776ae7b160915b71
brandmch <101467867+brandmch@users.noreply.github.com>
2023-01-26 14:41:38 -0500

    certifications.js

---
+++ src/routes/_landing/components/certifications.js
@@ -0,0 +1,54 @@
+import { Paper, Box, Typography } from "@mui/material";
+
+const Certifications = () => {
+  return (
+    <Box marginBottom={4} marginTop={4}>
+      <Paper elevation={4}>
+        <Box padding={2.5}>
+          <Typography variant="h4" align="center" gutterBottom>
+            Certifications
+          </Typography>
+          <hr />
+          <Typography gutterBottom marginTop={3}>
+            Back End Development and APIs{" "}
+            <a
+              href="https://www.freecodecamp.org/certification/brandmch/back-end-development-and-apis"
+              target="_blank"
+            >
+              (FCC)
+            </a>
+          </Typography>
+          <Typography gutterBottom>
+            Front End Development Libraries{" "}
+            <a
+              href="https://www.freecodecamp.org/certification/brandmch/front-end-development-libraries"
+              target="_blank"
+            >
+              (FCC)
+            </a>
+          </Typography>
+          <Typography gutterBottom>
+            Responsive Web Design{" "}
+            <a
+              href="https://www.freecodecamp.org/certification/brandmch/responsive-web-design"
+              target="_blank"
+            >
+              (FCC)
+            </a>
+          </Typography>
+          <Typography gutterBottom>
+            JavaScript Algorithms and Data Structures{" "}
+            <a
+              href="https://www.freecodecamp.org/certification/brandmch/javascript-algorithms-and-data-structures"
+              target="_blank"
+            >
+              (FCC)
+            </a>
+          </Typography>
+        </Box>
+      </Paper>
+    </Box>
+  );
+};
+
+export default Certifications;
--- src/routes/_landing/landing.js
+++ src/routes/_landing/landing.js
@@ -6,57 +6,7 @@ import useWindowSize from "../../utils/useWindowSize";
 
 import ContactInfo from "./components/contactInfo";
 import Skills from "./components/skills";
-
-const Certifications = () => {
-  return (
-    <Box marginBottom={4} marginTop={4}>
-      <Paper elevation={4}>
-        <Box padding={2.5}>
-          <Typography variant="h4" align="center" gutterBottom>
-            Certifications
-          </Typography>
-          <hr />
-          <Typography gutterBottom marginTop={3}>
-            Back End Development and APIs{" "}
-            <a
-              href="https://www.freecodecamp.org/certification/brandmch/back-end-development-and-apis"
-              target="_blank"
-            >
-              (FCC)
-            </a>
-          </Typography>
-          <Typography gutterBottom>
-            Front End Development Libraries{" "}
-            <a
-              href="https://www.freecodecamp.org/certification/brandmch/front-end-development-libraries"
-              target="_blank"
-            >
-              (FCC)
-            </a>
-          </Typography>
-          <Typography gutterBottom>
-            Responsive Web Design{" "}
-            <a
-              href="https://www.freecodecamp.org/certification/brandmch/responsive-web-design"
-              target="_blank"
-            >
-              (FCC)
-            </a>
-          </Typography>
-          <Typography gutterBottom>
-            JavaScript Algorithms and Data Structures{" "}
-            <a
-              href="https://www.freecodecamp.org/certification/brandmch/javascript-algorithms-and-data-structures"
-              target="_blank"
-            >
-              (FCC)
-            </a>
-          </Typography>
-        </Box>
-      </Paper>
-    </Box>
-  );
-};
+import Certifications from "./components/certifications";
 
 const Projects = () => {
   return (
