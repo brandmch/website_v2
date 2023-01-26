914ebf4d4e06e882da9161512557e5ef08a72ae5
brandmch <101467867+brandmch@users.noreply.github.com>
2023-01-25 22:02:07 -0500

    summary

--- src/routes/landing.js
+++ src/routes/landing.js
@@ -193,7 +193,28 @@ const Landing = () => {
                   Welcome!
                 </Typography>
                 <hr />
-                <Typography marginTop={3}>Some stuff...</Typography>
+                <Box display="flex" alignItems="center">
+                  <Box flex={10}>
+                    <Typography marginTop={3} color="white">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                      Duis aute irure dolor in reprehenderit in voluptate velit
+                      esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                      sint occaecat cupidatat non proident, sunt in culpa qui
+                      officia deserunt mollit anim id est laborum.
+                    </Typography>
+                  </Box>
+                  <Box flex={1}></Box>
+                  <Box
+                    flex={10}
+                    height={200}
+                    width={200}
+                    borderRadius={100}
+                    backgroundColor="white"
+                  ></Box>
+                </Box>
               </Box>
             </Paper>
           </Box>
