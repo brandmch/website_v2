1855eff2655c549d73b9d7bdbc112c2b822cadee
brandmch <101467867+brandmch@users.noreply.github.com>
2023-01-30 19:00:00 -0500

    hobbies

--- src/components/appbar.js
+++ src/components/appbar.js
@@ -10,6 +10,7 @@ import useWindowSize from "../utils/useWindowSize";
 
 const pages = [
   ["LinkedIn", "https://www.linkedin.com/in/mchugh-brandon/"],
+  ["GitHub", "https://github.com/brandmch"],
   // ["Projects", "/projects"],
   // ["Contact Me", "/contactme"],
 ];
@@ -39,6 +40,7 @@ const AppBarCustom = () => {
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <Typography
+            paddingLeft={width > 900 ? 8 : 0}
             variant="h4"
             noWrap
             component="a"
@@ -84,6 +86,7 @@ const AppBarCustom = () => {
           )}
           {width > 900 ? (
             <Box
+              paddingRight={8}
               sx={{
                 flexGrow: 1,
                 display: { xs: "none", md: "flex", justifyContent: "flex-end" },
--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import { Box, Typography } from "@mui/material";
 
 const FooterCustom = ({ url }) => {
   return (
-    <Box textAlign="center" marginBottom={1}>
+    <Box textAlign="center" paddingBottom={1}>
       <Typography>Brandon McHugh 2023</Typography>
       <Typography>
         <a href={url} target="_blank">
