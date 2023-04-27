0fe44fbddf82d720c6e38e2c7a5018ab01340769
brandmch <101467867+brandmch@users.noreply.github.com>
2023-04-27 10:23:53 -0400

    push

--- src/routes/social/auth/utils.js
+++
@@ -1,7 +0,0 @@
-import { getCurrentUser } from "./methods/currentUser";
-import { signUp } from "./methods/signup";
-import { confirmSignUp } from "./methods/confirm";
-import { signIn } from "./methods/login";
-import { signOut } from "./methods/signout";
-
-export { getCurrentUser, signUp, confirmSignUp, signIn, signOut };
