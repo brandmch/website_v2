6a0b228f19e88b94063e0b50121d9122ecdce83d
brandmch <101467867+brandmch@users.noreply.github.com>
2023-04-30 15:03:04 -0400

    css good, i think

--- src/routes/social/components/components/posts.js
+++ src/routes/social/components/components/posts.js
@@ -15,8 +15,31 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import CloseIcon from "@mui/icons-material/Close";
 import CheckIcon from "@mui/icons-material/Check";
 import { deletePost, editPost, sparsePost } from "../../hasura/utils";
+import { randomColor } from "../../../../utils/randomColor";
 
 export const Posts = ({ posts, user, setLoadPosts }) => {
+  const onHover = () => {
+    return {
+      "&:hover": {
+        color: `${randomColor()}`,
+      },
+    };
+  };
+
+  const IconBox = ({ icons }) => {
+    let [[callback1, icon1], [callback2, icon2]] = icons;
+    return (
+      <Box marginLeft={-1}>
+        <IconButton onClick={callback1} sx={onHover}>
+          {icon1}
+        </IconButton>
+        <IconButton onClick={callback2} sx={onHover}>
+          {icon2}
+        </IconButton>
+      </Box>
+    );
+  };
+
   const Paragraphs = ({ text }) => {
     return text.map((c) => (
       <Typography key={randomKeyGenerator()} fontSize={20}>
@@ -58,14 +81,12 @@ export const Posts = ({ posts, user, setLoadPosts }) => {
           fullWidth
           onChange={(e) => setInput(e.target.value)}
         />
-        <Box>
-          <IconButton onClick={() => setEdit(false)}>
-            <CloseIcon />
-          </IconButton>
-          <IconButton onClick={startEditPost}>
-            <CheckIcon />
-          </IconButton>
-        </Box>
+        <IconBox
+          icons={[
+            [() => setEdit(false), <CloseIcon fontSize="small" />],
+            [startEditPost, <CheckIcon fontSize="small" />],
+          ]}
+        />
       </Box>
     );
   };
@@ -74,58 +95,56 @@ export const Posts = ({ posts, user, setLoadPosts }) => {
     const [edit, setEdit] = useState(false);
     const [confirmDelete, setConfirmDelete] = useState(false);
     const { id, text, time, Posts_User, mine } = props;
+
     return (
       <Paper
         elevation={2}
         sx={{ padding: 3, borderRadius: 3, marginBottom: 3 }}
         key={id}
       >
-        <Typography>{Posts_User.name}</Typography>
-        <Typography>{parseDate(time)}</Typography>
+        <Typography fontSize={16} marginBottom={1}>
+          {Posts_User.name}
+        </Typography>
         {!edit ? (
-          <Box>
+          <Box marginY={1}>
             <Paragraphs text={text} />
-            {mine && (
-              <Box>
-                {!confirmDelete ? (
-                  <Box>
-                    <IconButton id="EDIT" onClick={() => setEdit(!edit)}>
-                      <EditIcon />
-                    </IconButton>
-                    <IconButton
-                      id="DELETE"
-                      onClick={() => setConfirmDelete(true)}
-                    >
-                      <DeleteIcon />
-                    </IconButton>
-                  </Box>
-                ) : (
-                  <Box>
-                    <Box display="flex" alignItems="center">
-                      <Typography>You sure?</Typography>
-                      <IconButton onClick={() => setConfirmDelete(false)}>
-                        <CloseIcon />
-                      </IconButton>
-                      <IconButton
-                        onClick={() =>
-                          deletePost(id).then((x) => setLoadPosts(true))
-                        }
-                      >
-                        <CheckIcon />
-                      </IconButton>
-                    </Box>
-                  </Box>
-                )}
-              </Box>
-            )}
           </Box>
         ) : (
           <EditPost setEdit={setEdit} text={text} id={id} />
         )}
-        <Divider
-          sx={{ backgroundColor: "black", margin: "5px 400px 5px 5px" }}
-        />
-        <Typography>-{Posts_User.username}</Typography>
+        <Divider sx={{ backgroundColor: "#ADADAD" }} />
+        <Typography fontSize={12}>{parseDate(time)}</Typography>
+        {mine && (
+          <Box>
+            {!confirmDelete ? (
+              <IconBox
+                icons={[
+                  [() => setEdit(!edit), <EditIcon fontSize="small" />],
+                  [
+                    () => setConfirmDelete(true),
+                    <DeleteIcon fontSize="small" />,
+                  ],
+                ]}
+              />
+            ) : (
+              <Box display="flex" alignItems="center">
+                <Typography>You sure?</Typography>
+                <IconBox
+                  icons={[
+                    [
+                      () => setConfirmDelete(false),
+                      <CloseIcon fontSize="small" />,
+                    ],
+                    [
+                      () => deletePost(id).then((x) => setLoadPosts(true)),
+                      <CheckIcon fontSize="small" />,
+                    ],
+                  ]}
+                />
+              </Box>
+            )}
+          </Box>
+        )}
       </Paper>
     );
   };
