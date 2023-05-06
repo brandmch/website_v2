c5c22ccb5ba47bb4dc93106a367cdc2f7c67529e
brandmch <101467867+brandmch@users.noreply.github.com>
2023-05-06 15:18:36 -0400

    color picker

--- src/routes/social/screens/settings.js
+++ src/routes/social/screens/settings.js
@@ -1,10 +1,50 @@
-import { Box, Typography, Button, Switch } from "@mui/material";
+import { useState } from "react";
+import { Box, Typography, Button, Paper } from "@mui/material";
 import { useParams } from "react-router-dom";
+import { HexColorPicker } from "react-colorful";
 
 //nightmode, changeColor, changePassword, deleteAccount
 
 const Settings = () => {
+  const [color, setColor] = useState("#0046FF");
+  const [showColors, setShowColors] = useState(false);
+  console.log(color);
+
   let userid = useParams().userid;
+
+  const changePassword = () => {
+    window.location.href = `/social/changepassword`;
+  };
+
+  const deleteAccount = () => {
+    window.location.href = `/social/deleteuserconfirmation/${userid}`;
+  };
+
+  const options = [
+    ["Change color", () => {}],
+    ["Change password", changePassword],
+    ["Delete account", deleteAccount],
+  ];
+
+  const Option = ({ x }) => {
+    const [title, callback] = x;
+    return (
+      <Box
+        display="flex"
+        alignItems="center"
+        flex={1}
+        minWidth={200}
+        key={title}
+      >
+        <Box flex={5}>
+          <Typography>{title}</Typography>
+        </Box>
+        <Box flex={1}>
+          <Button onClick={callback}>GO</Button>
+        </Box>
+      </Box>
+    );
+  };
 
   return (
     <Box
@@ -15,37 +55,49 @@ const Settings = () => {
       justifyContent="center"
       alignItems="center"
     >
-      <Typography color="text.primary">SETTINGS</Typography>
-      <Box display="flex" alignItems="center">
-        <Typography color="text.primary">night mode</Typography>
-        <Switch
-        // onChange={() => changeDarkMode(params.userid, params.darkmode)}
-        />
-      </Box>
-      <Box>
-        <Typography color="text.primary">change color</Typography>
-      </Box>
-      <Box display="flex" alignItems="center">
-        <Typography color="text.primary">changepassword</Typography>
-        <Button
-          onClick={() => {
-            window.location.href = `/social/changepassword`;
-          }}
-        >
-          GO
-        </Button>
-      </Box>
-      <Box display="flex" alignItems="center">
-        <Typography color="text.primary">delete account</Typography>
-        <Button
-          onClick={() => {
-            window.location.href = `/social/deleteuserconfirmation/${userid}`;
-          }}
-        >
-          GO
-        </Button>
-      </Box>
+      <Paper
+        sx={{
+          padding: 3,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h4" gutterBottom>
+          Settings
+        </Typography>
+        {options.map((curr) => (
+          <Option x={curr} />
+        ))}
+      </Paper>
+      <HexColorPicker color={color} onChange={setColor} />
     </Box>
   );
 };
 export default Settings;
+{
+  /* <Typography color="text.primary">SETTINGS</Typography>
+<Box>
+  <Typography color="text.primary">change color</Typography>
+</Box>
+<Box display="flex" alignItems="center">
+  <Typography color="text.primary">changepassword</Typography>
+  <Button
+    onClick={() => {
+      window.location.href = `/social/changepassword`;
+    }}
+  >
+    GO
+  </Button>
+</Box>
+<Box display="flex" alignItems="center">
+  <Typography color="text.primary">delete account</Typography>
+  <Button
+    onClick={() => {
+      window.location.href = `/social/deleteuserconfirmation/${userid}`;
+    }}
+  >
+    GO
+  </Button>
+</Box> */
+}
