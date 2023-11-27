9cfd73d9dcd307f73d8f42be9f859271e26890f4
brandmch <101467867+brandmch@users.noreply.github.com>
2023-11-27 17:36:28 -0500

    add task & todo

--- src/routes/superSimpleScrum/columns/scrumBoard.js
+++ src/routes/superSimpleScrum/columns/scrumBoard.js
@@ -1,5 +1,29 @@
-import { Box, Typography } from "@mui/material";
-const Task = ({ data }) => {
+import { useState, useEffect } from "react";
+import { Box, Typography, Button, TextField } from "@mui/material";
+
+const Task = ({ data, state, index }) => {
+  const [input, setInput] = useState("");
+
+  const handleClick_newTodo = () => {
+    let tempState = [...state.data];
+
+    tempState[index].todos = [input, ...tempState[index].todos];
+    const categories = ["tasks", "todos", "doings", "dones"];
+
+    categories.forEach((category) => {
+      tempState[index][category] = tempState[index][category].reduce(
+        (acc, curr) => (curr === "" ? acc : [...acc, curr]),
+        []
+      );
+    });
+
+    // remove "" from arrays
+    console.log(tempState);
+
+    state.setData(tempState);
+    setInput("");
+  };
+
   return data === "" ? (
     <Box height={152} marginBottom={1.5} padding={1} margin={1.5} />
   ) : (
@@ -13,12 +37,20 @@ const Task = ({ data }) => {
       margin={1.5}
     >
       <Typography>{data}</Typography>
+      {state && (
+        <Box>
+          <TextField
+            label="Add task"
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <Button variant="contained" onClick={handleClick_newTodo}></Button>
+        </Box>
+      )}
     </Box>
   );
 };
 
-const Story = ({ data }) => {
-  console.log(data);
+const Story = ({ data, state, index }) => {
   return (
     <Box
       border="1px solid green"
@@ -27,18 +59,22 @@ const Story = ({ data }) => {
       marginBottom={5}
     >
       {data.map((task, i) => (
-        <Task data={task} key={i} />
+        <Task data={task} state={state} key={i} index={index} />
       ))}
     </Box>
   );
 };
 
-const ScrumBoard = ({ storyData }) => {
+const ScrumBoard = ({ storyData, state, category }) => {
   return (
     <Box>
       {storyData.map((data, i) => (
         <Box key={i}>
-          <Story data={data} />
+          <Story
+            data={data}
+            state={category === "tasks" ? state : null}
+            index={i}
+          />
         </Box>
       ))}
     </Box>
--- src/routes/superSimpleScrum/main.js
+++ src/routes/superSimpleScrum/main.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, TextField, Button } from "@mui/material";
 import { ScrumBoard } from "./columns/scrumBoard";
 const data = require("./testData.json");
 const fakeData = [
@@ -39,30 +39,60 @@ function padData(storyData) {
 }
 
 const SimpleScrumMain = () => {
-  const newData = fakeData.map((curr) => {
-    return padData(curr);
-  });
-  const categorizedData = newData.reduce(
-    (acc, curr) => {
-      let tempObj = { ...acc };
-      tempObj.tasks = [...tempObj.tasks, curr.tasks];
-      tempObj.todos = [...tempObj.todos, curr.todos];
-      tempObj.doings = [...tempObj.doings, curr.doings];
-      tempObj.dones = [...tempObj.dones, curr.dones];
-      return tempObj;
-    },
-    { tasks: [], todos: [], doings: [], dones: [] }
-  );
+  const [input, setInput] = useState("");
+  const [data, setData] = useState(fakeData);
+  const state = {
+    input: input,
+    setInput: setInput,
+    data: data,
+    setData: setData,
+  };
+
+  const handleClick_newTask = () => {
+    setData([
+      ...data,
+      {
+        tasks: [input],
+        todos: [],
+        doings: [],
+        dones: [],
+      },
+    ]);
+    setInput("");
+  };
+
+  const categorizedData = data
+    .map((curr) => {
+      return padData(curr);
+    })
+    .reduce(
+      (acc, curr) => {
+        let tempObj = { ...acc };
+        tempObj.tasks = [...tempObj.tasks, curr.tasks];
+        tempObj.todos = [...tempObj.todos, curr.todos];
+        tempObj.doings = [...tempObj.doings, curr.doings];
+        tempObj.dones = [...tempObj.dones, curr.dones];
+        return tempObj;
+      },
+      { tasks: [], todos: [], doings: [], dones: [] }
+    );
 
   return (
     <Box>
       <Typography>Triple D Scrum</Typography>
+      <TextField label="Add task" onChange={(e) => setInput(e.target.value)} />
+      <Button onClick={handleClick_newTask} variant="contained" />
       <Box display="flex">
         {categories.map((cat, i) => (
           <Box width={250} textAlign="center" padding={2} key={i}>
             <Typography>{cat}</Typography>
             <Box border="1px solid black" borderRadius={1}>
-              <ScrumBoard storyData={categorizedData[cat]} key={i} />
+              <ScrumBoard
+                category={cat}
+                storyData={categorizedData[cat]}
+                state={state}
+                index={i}
+              />
             </Box>
           </Box>
         ))}
