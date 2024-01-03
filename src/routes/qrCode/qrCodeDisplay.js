cbf6688f182285e6ca18229e3c09759d6bb3ef77
brandmch <101467867+brandmch@users.noreply.github.com>
2024-01-03 17:05:00 -0500

    wow

--- src/routes/qrCode/qrCodeDisplay.js
+++ src/routes/qrCode/qrCodeDisplay.js
@@ -4,10 +4,20 @@ import { useParams } from "react-router-dom";
 
 export const QRCodeMain = () => {
   const { message } = useParams();
+  const [{ width, height }, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
 
   return (
-    <Box display="flex" alignItems="center" justifyContent="center" height="100vh">
-      <Typography variant="h1">{message}</Typography>
+    <Box display="flex" alignItems="center" justifyContent="center" height="100vh" maxWidth="100vw">
+      <Typography
+        variant={width < 350 ? "h3" : width < 700 ? "h2" : "h1"}
+        padding={3}
+        style={{ wordBreak: "break-word", hyphens: "auto" }}
+      >
+        {message}
+      </Typography>
     </Box>
   );
 };
